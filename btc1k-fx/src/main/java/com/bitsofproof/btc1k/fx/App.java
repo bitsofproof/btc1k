c955cd5c18661f58836fefa5cf2c552153ded70e
Bártfai Tamás <tamas.bartfai@bitsofproof.com>
2013-11-29 19:30:11 +0100

    Added javafx skeleton

---
+++ btc1k-fx/src/main/java/com/bitsofproof/btc1k/fx/App.java
@@ -0,0 +1,18 @@
+package com.bitsofproof.btc1k.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+	public static void main (String[] args)
+	{
+		launch (args);
+	}
+
+	@Override
+	public void start (Stage primaryStage) throws Exception
+	{
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
