af2d4410d0bace77da4e4630340c8306292eda4e
Tamas Blummer <tamas@bitsofproof.com>
2013-12-06 20:03:51 +0100

    also log address

--- btc1k-server/src/main/java/com/bitsofproof/btc1k/server/resource/BopShopResource.java
+++ btc1k-server/src/main/java/com/bitsofproof/btc1k/server/resource/BopShopResource.java
@@ -159,7 +159,7 @@ public class BopShopResource
 			input.setScript (sw.toByteArray ());
 		}
 		transaction.computeHash ();
-		log.info ("Ticket issued with " + transaction.getHash () + " for " + request.getId ());
+		log.info ("Ticket issued with " + transaction.getHash () + " for " + request.getId () + " address " + request.getTitle ());
 		api.sendTransaction (transaction);
 	}
 
